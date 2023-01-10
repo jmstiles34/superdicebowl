aa5d65a35dd6034ae6b9a403b75cce97c4b02d4f
Jarrod Stiles <jstiles@lunchbox.io>
2023-01-10 11:39:08 -0500

    Add initial files for repo

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,28 @@
+<script>
+    //import Title from '$lib/title.svelte'
+    import '../styles/global.css'
+  </script>
+  
+  <header>
+    <h1>HELLO</h1>
+  </header>
+  <main>
+    <slot></slot>
+  </main>
+  <footer>
+    <p>Copyright 2022 Ninja Gaming Guides</p>
+  </footer>
+  
+  <style>
+    header {
+      display: flex;
+      justify-content: center;
+    }
+    main {
+      max-width: 960px;
+      margin: 20px auto;
+    }
+    footer {
+      text-align: center;
+    }
+  </style>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,15 @@
+<script>
+    let teamsJson = fetch('/teams.json')
+    .then(res => res.json())
+    .then((data) => {
+    console.log(data)
+  });
+</script>
+
+<main>
+<img alt="Dallas Cowboys" src='/logos/Cowboys.png'/>
+</main>
+
+<style>
+
+</style>
