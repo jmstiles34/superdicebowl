439d932b3df3d34b5f3b49026a09e966db14e4d5
Jarrod Stiles <jstiles@lunchbox.io>
2023-07-03 12:40:44 -0400

    Update to svelte4

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,12 +1,27 @@
 module.exports = {
 	root: true,
 	parser: '@typescript-eslint/parser',
-	extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'prettier'],
-	plugins: ['svelte3', '@typescript-eslint'],
+	extends: ['plugin:svelte/recommended', 'plugin:@typescript-eslint/recommended', 'prettier'],
+	plugins: ['@typescript-eslint'],
 	ignorePatterns: ['*.cjs'],
-	overrides: [{ files: ['*.svelte'], processor: 'svelte3/svelte3' }],
+	overrides: [{ files: ['*.svelte'], parser: 'svelte-eslint-parser', }],
 	settings: {
-		'svelte3/typescript': () => require('typescript')
+		svelte: {
+			ignoreWarnings: [
+				"@typescript-eslint/no-unsafe-assignment",
+				"@typescript-eslint/no-unsafe-member-access",
+			],
+			compileOptions: {
+				postcss: {
+					configFilePath: "./path/to/my/postcss.config.js",
+				},
+			},
+			kit: {
+				files: {
+					routes: "src/routes",
+				},
+			},
+		}
 	},
 	parserOptions: {
 		sourceType: 'module',
